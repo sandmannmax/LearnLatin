a220a67afbfda2e1037832c2c86898502dd8dec1
sandmannmax <sandmannmaxxer@gmail.com>
2017-11-26 15:35:37 +0100

    added EXIT function while beeing in while loop while inserting case or casefunction

--- LearnLatin.py
+++ LearnLatin.py
@@ -23,25 +23,37 @@ print()
 #Spiel startet
 print("Spiel beginnt jetzt!")
 print()
-randNum = random.randint(0,len(allDataList)-1)
-print(allDataList[randNum][2])
-#Kasusüberprüfung
 while True:
-    answer1In = input("Bitte gebe den vorliegenden Kasus ein: ")
-    if answer1In.upper() == allDataList[randNum][0].upper():
+    randNum = random.randint(0,len(allDataList)-1)
+    print(allDataList[randNum][2])
+    #Kasusüberprüfung
+    while True:
+        answer1In = input("Bitte gebe den vorliegenden Kasus ein: ")
+        if answer1In.upper() == 'EXIT':
+            break
+        elif answer1In.upper() == allDataList[randNum][0].upper():
+            break
+        else: 
+          print("Leider falsch! Überlege noch einmal genau.")
+    #Kasusfunktionsüberprüfung
+    while True:
+        answer2In = input("Bitte gebe nun die vorliegende Kasusfunktion ein (nur die Funktion ohne Kasus, z.B. als Attribut): ")
+        if answer2In.upper() == 'EXIT':
+            break 
+        elif answer2In.upper() == allDataList[randNum][1].upper():
+              break
+        else: 
+            print("Leider falsch! Überlege noch einmal genau.")
+    print("Gut gemacht!")
+    print()
+    print()
+    goOn = input("Fortsetzen? j/n: ")
+    while goOn != 'j' or goOn != 'n':
+        goOn = input("Fehler! Bitte noch einmal... j/n: ")
+    if goOn == 'n':
         break
-    else: 
-        print("Leider falsch! Überlege noch einmal genau.")
-#Kasusfunktionsüberprüfung
-while True:
-    answer2In = input("Bitte gebe nun die vorliegende Kasusfunktion ein (nur die Funktion ohne Kasus, z.B. als Attribut): ")
-    if answer2In.upper() == allDataList[randNum][1].upper():
-        break
-    else: 
-        print("Leider falsch! Überlege noch einmal genau.")
+    print()
 
-print("Gut gemacht!")
-print()
-print()
-print("----Max Sandmann, V 0.1 , 26.11.2017----")
+
+print("----Max Sandmann, V 0.1.1 , 26.11.2017----")
 
